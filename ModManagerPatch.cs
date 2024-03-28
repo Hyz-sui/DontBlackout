02c53bb28e110857b05644515d1d1620ba956362
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2024-03-29 01:12:16 +0900

    format

--- ModManagerPatch.cs
+++ ModManagerPatch.cs
@@ -1,6 +1,6 @@
 namespace DontBlackout;
 
-[HarmonyPatch(typeof(ModManager),nameof(ModManager.LateUpdate))]
+[HarmonyPatch(typeof(ModManager), nameof(ModManager.LateUpdate))]
 public static class ModManagerLateUpdatePatch
 {
     public static void Postfix(ModManager __instance)
